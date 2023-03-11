f0ffe97f9e66e6cd596162e0134fd75bc1b1c0c5
Alan Ibatullin <mr.juneiak@gmail.com>
2023-03-11 10:12:52 +0500

    try del logo

--- src/updated/components/common/Logo/Logo.jsx
+++ src/updated/components/common/Logo/Logo.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as styles from './logo.module.css';
-import logoImage from '../../../images/misc/logo-main.svg'
+import logoImage from './logo-main.svg'
 
 export default function Logo() {
   return <img src={logoImage} alt="trafflab logo" className={styles.logo} />
--- src/updated/components/common/index.js
+++ src/updated/components/common/index.js
@@ -1,12 +1,19 @@
+import * as React from "react"
+
 import SiteLayout from "./site-layout";
 import PartnersMarquee from "./partners-marquee/partners-marquee";
 import CountriesMarquee from "./countries-marquee/countries-marquee";
 import SliderLayout from "./slider-layout/slider-layout";
 import SectionContentLayout from "./section-content-layout/section-content-layout";
 import AskQuestion from "./ask-question/ask-question";
-import Logo from "./logo/logo";
+// import Logo from "./logo/logo";
 import BlogCard from "./blog-card/blog-card";
 import BlogPageCard from "./blog-page-card/blog-page-card";
+
+
+function Logo() {
+  return <div/>
+}
 
 export {
   SiteLayout,
