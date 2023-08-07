fa7dbf9bebf6d61ad8b94d7e21caa49a1f9d0e45
vladkonst <vladkonst98@gmail.com>
2023-08-07 14:41:55 +0300

    changed captcha key

--- src/updated/components/common/recaptcha/recaptcha.jsx
+++ src/updated/components/common/recaptcha/recaptcha.jsx
@@ -14,7 +14,10 @@ export default function Recaptcha({ hl, handler = () => false, isOpen }) {
     <div data-is-open={isOpen} className={styles.cover}>
       <div className={styles.container}>
       <ReCAPTCHA
-        sitekey="6LecrIknAAAAAO5vaZwg6RVGFLR_UysRFOchclNp"
+        //site 6LecrIknAAAAAO5vaZwg6RVGFLR_UysRFOchclNp
+        //secret 6LecrIknAAAAACDMjkrNuOtCWQP0vXTHF32JXO4s
+        //old 6LcbvsUlAAAAACWvENvT_pX7fYvgdTzX4MGn9_v4
+        sitekey="6LecrIknAAAAACDMjkrNuOtCWQP0vXTHF32JXO4s"
         onChange={handleChange}
         hl={hl}
       />
