1fbc6ef6b1fd6b44bc95a5f106514531758b6e40
vladkonst <vladkonst98@gmail.com>
2023-09-27 18:51:21 +0300

    fixed admin bug

---
+++ src/cms/cms-utils.js
@@ -0,0 +1,13 @@
+if (typeof window !== 'undefined') {
+    const link = document.createElement('link')
+    link.type = 'text/css'
+    link.rel = 'stylesheet'
+    link.href = '/admin/admin.css'
+    document.head.appendChild(link)
+
+    if (process.env.NETLIFY_SITE_URL) {
+        window.localStorage.setItem(
+            'netlifySiteURL', process.env.NETLIFY_SITE_URL
+        )
+    }
+}
