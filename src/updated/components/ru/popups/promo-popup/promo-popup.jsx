f643eb78d1b9c56a67e4c9d88d44bccaa90ee7f8
vladkonst <vladkonst98@gmail.com>
2023-10-01 23:57:27 +0300

    22

---
+++ src/updated/components/ru/popups/promo-popup/promo-popup.jsx
@@ -0,0 +1,31 @@
+import * as React from "react"
+import * as styles from './promo-popup.module.css';
+import PopupLayout from "../popup-layout/popup-layout";
+import { SiteImage } from "../../../common/ui";
+import { Loader } from "../../../common/ui";
+
+export default function PromoPopup({isOpen, data, closeHandler}) {
+
+  // if (!data?.title) return null
+  return (
+    <PopupLayout isOpen={isOpen} closeHandler={closeHandler}>
+      <div className={styles.PromoPopup}>
+        <button onClick={closeHandler} className={styles.closeButton} />
+        {
+          !data?.title 
+          ? <Loader/>
+          : data.notReadyMessage 
+            ? <h3 className={styles.title}>{data.notReadyMessage}</h3>
+            : <div className={styles.content}>
+                <div className={styles.imageContainer}><SiteImage image={data.image} /></div>
+                <h3 className={styles.title}>{data.title}</h3>
+                <div className={styles.textContainer} dangerouslySetInnerHTML={{__html: data.text}} />
+                <p className={styles.date}>{data.date}</p>
+              </div>
+        }
+      </div>
+
+    </PopupLayout>
+
+  )
+}
